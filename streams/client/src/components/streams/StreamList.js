979a30444c834f0c3a7e099e9b275beae63e5057
Wytze Gelderloos <wytzepakito@gmail.com>
2021-01-21 10:09:08 +0100

    Merge branch 'master' of https://github.com/Wytzepakito/learningReact
    # Conflicts:
    #	.gitignore

--- streams/client/src/components/streams/StreamList.js
+++ streams/client/src/components/streams/StreamList.js
@@ -1,49 +1,56 @@
 import React from 'react';
-import {connect } from 'react-redux';
-import { fetchStreams} from "../../actions";
-import { Link } from 'react-router-dom';
+import {connect} from 'react-redux';
+import {fetchStreams} from "../../actions";
+import {Link} from 'react-router-dom';
 
 class StreamList extends React.Component {
   componentDidMount() {
     this.props.fetchStreams()
   }
-  renderAdmin(stream){
-    if (stream.userId=== this.props.currentId){
+
+  renderAdmin(stream) {
+    if (stream.userId === this.props.currentId) {
       return (
         <div className="right floated content">
-          <Link to={`/streams/edit/${stream.id}`} className="ui button primary">Edit</Link>
-          <button className="ui button negative">Delete</button>
+          <Link to={`/streams/edit/${stream.id}`} className="ui button primary">
+            Edit
+          </Link>
+          <Link to={`/streams/delete/${stream.id}`} className="ui negative button">
+            Delete
+          </Link>
+        </div>
+      )
+    }
+  }
+
+  renderCreate() {
+    if (this.props.isSignedIn) {
+      return (
+        <div style={{textAlign: 'right'}}>
+          <button className="ui button primary">
+            <Link to="/streams/new" style={{color: 'white'}}>Create Stream</Link>
+          </button>
         </div>
       )
     }
   }
 
 
-  renderList(){
+  renderList() {
     return this.props.streams.map(stream => {
       return (
         <div className="item" key={stream.id}>
           {this.renderAdmin(stream)}
-          <i className="large middle aligned icon camera" />
+          <i className="large middle aligned icon camera"/>
           <div className="content">
-            {stream.title}
+            <Link to={`/streams/${stream.id}`} className="header">
+              {stream.title}
+            </Link>
             <div className="description"> {stream.description}</div>
           </div>
         </div>
       )
     })
-  }
-
-  renderCreate(){
-    if (this.props.isSignedIn){
-        return (
-          <div style={{ textAlign: 'right'}} >
-            <button className="ui button primary">
-            <Link to="/streams/new" style={{ color: 'white' }}>Create Stream</Link>
-              </button>
-          </div>
-        )
-    }
   }
 
 
